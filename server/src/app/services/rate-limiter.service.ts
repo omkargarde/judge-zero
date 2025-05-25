dc2e6074a7126516ef8cc1eda9abcb778fb8ec8a
omyagarde <omyagarde@gmail.com>
2025-05-25 17:59:27 +0530

    fix: Correct comment for rate limiter and adjust formatting in schema

--- server/src/app/services/rate-limiter.service.ts
+++ server/src/app/services/rate-limiter.service.ts
@@ -1,10 +1,10 @@
 import rateLimit from 'express-rate-limit'
 
-// Rate limiter: maximum of 5 requests per minute for verifyUser
+// Rate limiter: maximum of 5 requests per minute per IP
 const RateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 5, // limit each IP to 5 requests per windowMs
   message: 'Too many requests attempts from this IP, please try again later.',
 })
 
-export { RateLimiter  }
+export { RateLimiter }
